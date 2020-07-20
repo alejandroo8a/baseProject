222f4aaf2d8ff2e31a3dfd78395548a68c8627ab
Alejandro Ochoa <alejandro.ochoa@metova.com>
2020-07-19 23:30:49 -0500

    Added coroutines

---
+++ app/src/main/java/com/alejandro8a/androidTemplate/presentation/base/BaseViewModel.kt
@@ -0,0 +1,21 @@
+package com.alejandro8a.androidTemplate.presentation.base
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+
+class BaseViewModel : ViewModel() {
+
+    val scope = CoroutineScope(
+        Job() + Dispatchers.Main
+    )
+
+    // Cancel the job when the view model is destroyed
+    override fun onCleared() {
+        super.onCleared()
+        scope.cancel()
+    }
+
+}
